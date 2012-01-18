c4386597d2e1b7e5cc40a262e0f67c8bc05e0d6d
Ma Bo <mabo.cn@gmail.com>
2012-01-18 16:30:54 +0800

    [spec] unit spec for conf dsl - done

--- spec/unit/conf_dsl_spec.rb
+++ spec/unit/conf_dsl_spec.rb
@@ -1,5 +1,84 @@
 require "spec_helper"
 
-describe "Deployment::DSL - CONF format" do
+describe "Deployment::ConfDSL" do
 
+  before(:all) do
+    @dsl = Deployment::ConfDSL.new
+  end
+
+  describe "add" do
+    it "one entry" do
+      @dsl.content = {}
+      platform_descriptor =<<-END
+        add :key => 'value'
+      END
+      expected = { 'key' => 'value' }
+
+      @dsl.instance_eval platform_descriptor
+      @dsl.content.should == expected
+    end
+
+    it "multi entries" do
+      @dsl.content = {}
+      platform_descriptor =<<-END
+        add :key_0 => 'val 0'
+        add :key_1 => 'val 1'
+      END
+      expected = { 'key_0' => 'val 0', 'key_1' => 'val 1' }
+
+      @dsl.instance_eval platform_descriptor
+      @dsl.content.should == expected
+    end
+  end
+
+  describe "upd" do
+
+    it "one entry" do
+      @dsl.content = { 'key_0' => 'val 0', 'key_1' => 'val 1' }
+      platform_descriptor =<<-END
+        upd :key_0 => 'upd 0'
+      END
+      expected = { 'key_0' => 'upd 0', 'key_1' => 'val 1' }
+
+      @dsl.instance_eval platform_descriptor
+      @dsl.content.should == expected
+    end
+
+    it "multi entries" do
+      @dsl.content = { 'key_0' => 'val 0', 'key_1' => 'val 1' }
+      platform_descriptor =<<-END
+        upd :key_0 => 'upd 0'
+        upd :key_1 => 'upd 1'
+      END
+      expected = { 'key_0' => 'upd 0', 'key_1' => 'upd 1' }
+
+      @dsl.instance_eval platform_descriptor
+      @dsl.content.should == expected
+    end
+  end
+
+  describe "del" do
+    it "one entry" do
+      @dsl.content = { 'key_0' => 'val 0', 'key_1' => 'val 1' }
+      platform_descriptor =<<-END
+        del :key_0
+      END
+      expected = { 'key_1' => 'val 1' }
+
+      @dsl.instance_eval platform_descriptor
+      @dsl.content.should == expected
+    end
+
+    it "multi entries" do
+      @dsl.content = { 'key_0' => 'val 0', 'key_1' => 'val 1' }
+      platform_descriptor =<<-END
+        del :key_0
+        del :key_1
+      END
+      expected = {}
+
+      @dsl.instance_eval platform_descriptor
+      @dsl.content.should == expected
+    end
+  end
 end
